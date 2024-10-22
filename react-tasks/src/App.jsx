236706489e58b8afe90264eb3a02bfe9467401c4
Fabregas14 <olusanyatee13@gmail.com>
2024-10-22 22:44:24 +0100

    first commit

---
+++ react-tasks/src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./homeone/Homeone"
+import Hometwo from "./hometwo/Hometwo"
+import Homeiii from "./homeiii/Homeiii"
+import Link from "./homeiii/Homeiii"
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Home/>             
+      <Hometwo/>
+      <Link/>
+    </div>
+  )
+}
+export default App
